669c02a451e57860e5cf4a878e16c4de9e2aeea1
Thiago Bonotto <thbonotto@gmail.com>
2016-02-22 21:38:50 -0300

    Evolução.

--- include/Event/Events.h
+++ include/Event/Events.h
@@ -3,7 +3,7 @@
 #include <string>
 #include <vector>
 //TODO Do this in one place only:
-typedef std::pair<std::string,std::string> Par;
+typedef std::pair<std::string, std::string> Par;
 
 class Event {
 public:
@@ -37,7 +37,8 @@ protected:
 class LoginReq: public Event {
 
 public:
-	LoginReq(std::string& username, std::string& password) : mUsername{username}, mPassword {password} {
+	LoginReq(std::string& username, std::string& password) :
+			mUsername { username }, mPassword { password } {
 		this->eventId = Event::EventType::LOGIN_REQ;
 	}
 	const std::string getUsername() const {
@@ -63,7 +64,8 @@ public:
 
 class LogoutReq: public Event {
 public:
-	LogoutReq(std::string& username) : mUsername{username} {
+	LogoutReq(std::string& username) :
+			mUsername { username } {
 		this->eventId = Event::EventType::LOGOUT_REQ;
 	}
 
@@ -86,8 +88,8 @@ public:
 
 class JoinReq: public Event {
 public:
-	JoinReq(std::string& username, std::string& match, std::string& extra)
-	: mUsername{username}, mMatch{match}, mExtra{extra} {
+	JoinReq(std::string& username, std::string& match, std::string& extra) :
+			mUsername { username }, mMatch { match }, mExtra { extra } {
 		this->eventId = Event::EventType::JOIN_REQ;
 	}
 
@@ -100,7 +102,7 @@ public:
 	}
 
 	const std::string getExtra() const {
-			return this->mExtra;
+		return this->mExtra;
 	}
 
 private:
@@ -119,7 +121,8 @@ public:
 
 class LeaveReq: public Event {
 public:
-	LeaveReq(std::string& username) : mUsername{username} {
+	LeaveReq(std::string& username) :
+			mUsername { username } {
 		this->eventId = Event::EventType::LEAVE_REQ;
 	}
 
@@ -174,12 +177,23 @@ public:
 
 class Data: public Event {
 public:
-	Data(std::string& username,	std::vector<Par>& data) : mUsername{username}, mData{data}  {
+	Data(std::string& username, std::vector<Par>& data, std::vector<Par>& resp) :
+			mUsername { username }, mData { data }, mResp { resp } {
 		this->eventId = Event::EventType::DATA;
 	}
+
+	const std::string getUsername() const {
+		return this->mUsername;
+	}
+
+	const std::string getData() const {
+		return this->mData;
+	}
+
 private:
 	std::string mUsername;
 	std::vector<Par> mData;
+	std::vector<Par>& mResp;
 
 };
 
@@ -193,9 +207,22 @@ public:
 
 class Subscribe: public Event {
 public:
-	Subscribe() {
+	Subscribe(std::string& username, std::string& assunto) :
+			mUsername { username }, mAssunto { assunto } {
 		this->eventId = Event::EventType::SUBSCRIBE;
 	}
+
+	const std::string getUsername() const {
+		return this->mUsername;
+	}
+
+	const std::string getAssunto() const {
+		return this->mAssunto;
+	}
+
+private:
+	std::string mUsername;
+	std::string mAssunto;
 
 };
 
@@ -209,8 +236,20 @@ public:
 
 class Notify: public Event {
 public:
-	Notify() {
+	Notify(std::string& username, std::vector<Par>& data) :
+			mUsername { username }, mData { data } {
 		this->eventId = Event::EventType::NOTIFY;
 	}
 
+	const std::string getUsername() const {
+		return this->mUsername;
+	}
+
+	const std::string getData() const {
+		return this->mData;
+	}
+
+private:
+	std::string mUsername;
+	std::vector<Par> mData;
 };
